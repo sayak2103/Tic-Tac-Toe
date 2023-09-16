90ac1462f71149624bad5711f75e7a510f9fac6f
Sayak_Chowdhury <sayak2103@gmail.com>
2023-09-16 11:52:51 +0530

    crreating check functions

--- appTTT.js
+++ appTTT.js
@@ -8,7 +8,8 @@ let hum=document.querySelector("#human");
 let ins=document.querySelector(".instruct");
 let gameMode;
 let symbol=["<i class='fa-solid fa-xmark fa-8x'></i>","<i class='fa-solid fa-o fa-7x'></i>"]// 
-let f=0;
+let isymb=0;
+let countx,counto,r,c;
 
 let arr=[
     [inboxes[0],inboxes[1],inboxes[2]],
@@ -23,6 +24,89 @@ let def=[[]];     //array to store all the possible defnsive moves
 let att=[[]];     //array to store all the possible attacking moves
 let id,ia;
 id=ia=1;
+
+const Check={    
+    row: function(f,a){
+        counto=countx=0;
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+                if(arr[i][j].title==f)
+                    countx++;
+                else if(arr[i][j].title!=a)
+                    {counto++;r=i;c=j;}    
+            
+            if(countx==2 && counto==1)
+        {att.push([r,c]);
+        ia++;}
+        else if(countx==3)
+        {status=false;
+        result=f;
+        }
+        countx=counto=0;
+        }
+    },
+    
+    column : function(f,a){
+        counto=countx=0;
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+                if(arr[j][i].title==f)
+                    countx++;
+                else if(arr[j][i].title!=a)
+                    {counto++;r=j;c=i;}    
+            
+            if(countx==2 && counto==1)
+        {att.push([r,c]);
+        ia++;}
+        else if(countx==3)
+        {status=false;
+        result=f;
+        }
+        countx=counto=0;
+        }
+    },
+
+    diagonanl : function(f,a){
+        counto=countx=0;
+        for(i=0;i<3;i++)
+            if(arr[i][i].title==f)
+                countx++;
+            else if(arr[i][i].title!=a)
+                {counto++;r=i;c=i;}
+    
+        if(countx==2 && counto==1)
+        {att.push([r,c]);
+        ia++;
+        }
+        else if(countx==3)
+        {status=false;
+        result=f;
+        }
+        countx=counto=0;
+
+        for(i=0,j=2;i<3;i++,j--)
+        {
+            if(arr[j][i].title==f)
+                countx++;
+            else if(arr[j][i].title!=a)
+                {counto++;r=j;c=i;}
+        }
+
+        if(countx==2 && counto==1)
+        {att.push([r,c]);
+        ia++;
+        }
+        else if(countx==3)
+        {status=false;
+        result=f;
+        }
+        countx=counto=0;
+    }
+}
+
+
 
 start.addEventListener("click",()=>{
     for(inbox of inboxes)
